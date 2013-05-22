ededa415c9ae828a8e0b9a631705c3c3230378bd
Allison Wintrip <awintrip@playnetwork.com>
2013-05-22 16:25:31 -0700

    Apparently, the last commit didn't include the updated ssa.py.  Let's try this again.  Removed intermediate step of converting TDF to JSON file.  Now pushing directly into ElasticSearch DB.

--- src/search-sync-agent/ssa.py
+++ src/search-sync-agent/ssa.py
@@ -4,7 +4,7 @@ import time
 
 import argparse
 import csv
-import json
+#import json
 
 import pyes
 
@@ -12,14 +12,47 @@ import pyes
 #
 # TODO:
 # - change key names to something more understandable
-# - for readability, perhaps break out argparser and extensions string
-#   setup into their own method?
 #
 
 
 
-# UTF-8 files generated on windows machines generally start with a byte-order
-# mark.  We need to remove the BOM so the dictreader doesn't use it as part
+def add_tdf_to_elasticsearch(filename, es_server, index_name=None, delete_preexisting_index=False):
+    remove_bom_from_utf8(filename)
+    
+    if index_name is None:
+        filename_no_path = filename.split('\\')[-1]
+        index_name = filename_no_path.split('.')[0].lower()
+    
+    conn = pyes.ES(es_server)
+    index_is_ready = verify_es_index(index_name, conn, delete_preexisting_index)
+
+    sys.stdout.write("Adding TDF data to ElasticSearch...")
+    sys.stdout.flush()
+
+    start_time = time.time()
+    if index_is_ready:
+        doc_count = 0
+        with open(filename, 'rb') as tdf_file:
+            reader = csv.DictReader(tdf_file, delimiter='\t')
+
+            for row in reader:
+                # for now, index name matches the type
+                conn.index(row, index_name, index_name)
+                
+                doc_count += 1
+                if (doc_count % 1000) == 0:
+                    sys.stdout.write(".")
+                    sys.stdout.flush()
+
+        conn.indices.refresh(index_name)
+
+    elapsed_time = time.time() - start_time
+    print "done!  Completed in " + str(elapsed_time) + " seconds."
+
+
+
+# UTF-8 files generated on Windows machines generally start with a byte-order
+# mark.  We need to remove the BOM so the DictReader doesn't use it as part
 # of a column header.  If it doesn't have the BOM, this won't hurt anything.
 def remove_bom_from_utf8(filename):
     with open(filename, 'rb') as f:
@@ -54,75 +87,35 @@ def remove_bom_from_utf8(filename):
 
 
 #
-# Converts the specified file to JSON format.
+# Adds the contents of all files in a directory with the specified extension(s)
+# to an ES database.
+# Assumption: The original files are TDF format.
 #
-# Assumptions:
-#   - Specified file is a tab-delimited file (TDF)
-#   - It's OK to overwrite any existing files in that directory
-#     that have the same name except a .json extension.
-#
-def tdf_to_json (filename):
-    method_start_time = time.time()
-
-    remove_bom_from_utf8(filename)
-
-    with open(filename, 'rb') as tdf:
-        sys.stdout.write("reading in TDF... ")  # print next info on same line
-        sys.stdout.flush()
-
-        start_time = time.time()
-
-        reader = csv.dictreader(tdf, delimiter='\t')
-        data = json.dumps([r for r in reader], indent=4, separators=(',', ': '))
-
-        elapsed_time = time.time() - start_time
-        print "done!  Completed in " + str(elapsed_time) + " seconds."
-    
-    json_filename = filename.split('.')[0] + '.json'
-
-    with open(json_filename, 'w') as json_file:
-        sys.stdout.write("Writing JSON file... ")
-        sys.stdout.flush()
-
-        start_time = time.time()
-
-        json_file.write(data)
-
-        elapsed_time = time.time() - start_time
-        print "done!  Completed in " + str(elapsed_time) + " seconds."
-
-    method_elapsed_time = time.time() - method_start_time
-    print "Done converting TDF to JSON!"
-    print "The conversion for " + filename + " took " + \
-          str(method_elapsed_time) + " seconds."
-    print "JSON data can be found at " + json_filename + "."
-
-
-
-#
-# Converts all files in a directory with the specified extension to
-# JSON format.  It is assumed that the original files are TDF format.
-#
-def convert_all_tdf_in_dir(path, extension=('.txt', '.tdf')):
-    convert_all_start_time = time.time()
+def add_tdfs_in_dir_to_elasticsearch(path, es_server, extensions=('.txt', '.tdf'), \
+                             delete_preexisting_indices=False):
+    add_all_start_time = time.time()
     
     directory = os.listdir(path)
 
     if not path.endswith("\\"):
         path += "\\"
 
-    files_converted_count = 0
+    files_added_count = 0
     
     for f in directory:
-        if f.endswith(extension):
+        if f.endswith(extensions):
             print f
-            tdf_to_json(path + f)
+            add_tdf_to_elasticsearch(path + f, es_server, \
+                    delete_preexisting_index=delete_preexisting_indices)
             files_converted_count += 1
             print ""
     
-    convert_all_elapsed_time = time.time() - convert_all_start_time
-    print "Converting " +str(files_converted_count) + " files took " + \
-          str(convert_all_elapsed_time) + " seconds."
+    add_all_elapsed_time = time.time() - add_all_start_time
+    print "Adding data from " +str(files_added_count) + \
+          " files to ElasticSearch took " + str(add_all_elapsed_time) + \
+          " seconds."
+
+    return files_converted_count
 
 
 
@@ -145,60 +138,10 @@ def verify_es_index(index_name, conn, delete_existing_index=False):
 
 
 #
-# Processes the specified JSON file and pushes the JSON data into
-# the specified ElasticSearch database.
+# Dev test method; not meant for inclusion in non-dev branches.
 #
-# Assumption: the JSON file's name, minus the extension, is also the
-# name of the index that should contain the data.
-#
-def send_json_to_elasticsearch(filename, es_ip):
-    start_time = time.time()
-    sys.stdout.write("Reading in JSON file... ")  # print next info on same line
-    sys.stdout.flush()
-
-    with open(filename, 'rb') as json_file:
-        data = json.load(json_file)
-
-    elapsed_time = time.time() - start_time
-    print "done!  Completed in " + str(elapsed_time) + " seconds."
-    
-    filename_no_path = filename.split("\\")[-1]
-    index_name = filename_no_path.split(".")[0].lower()
-    #print index_name
-    
-    conn = pyes.ES(es_ip)
-    delete_index_first = True
-    index_exists = verify_es_index(index_name, conn, \
-            delete_existing_index=delete_index_first)
-
-    start_time = time.time()
-    sys.stdout.write("Adding JSON data to index " + index_name + "... ")  # print next info on same line
-    sys.stdout.flush()
-
-    if index_exists:
-        try:
-            for row in data:
-                # for now, index name matches the type
-                conn.index(row, index_name, index_name)
-            conn.indices.refresh(index_name)
-
-        except:
-            print "unexpected error: ", sys.exc_info()[0]
-    else:
-        print "\nCould not add data to ElasticSearch; index '" + index_name + \
-              "' does not exist!"
-    
-    elapsed_time = time.time() - start_time
-    print "done!  Completed in " + str(elapsed_time) + " seconds."
-    
-
-
-#
-# Runs a simple query to verify existence of data in an index.
-# Dev testbed method; not meant for inclusion in non-dev branches.
-#
-def test_es(es_ip):
-    conn = pyes.ES(es_ip)
+def test_es(es_server):
+    conn = pyes.ES(es_server)
 
     q = pyes.TermQuery("Is_A_Timezone_Folder", False)   # they're all false, this is the easiest way to get all records.
     results = conn.search(query=q)
@@ -212,6 +155,7 @@ def test_es(es_ip):
         print "Printed the first " + str(max_to_print) + " results."
 
     print str(num_results) + " results found!"
+    print conn.get_indices()
 
 
 
@@ -228,38 +172,42 @@ def set_up_argparser():
             if len(default_input_extensions) > 1:
                 default_extensions_string += "; "
 
-    parser = argparse.ArgumentParser(description='Process TDF and JSON files, \
-                                     and send them to an ElasticSearch database.')
+    parser = argparse.ArgumentParser(description='Process TDF files and add \
+                                     them to an ElasticSearch database.')
+
     group = parser.add_mutually_exclusive_group(required=True)
+
     group.add_argument('-f', '--files', nargs='+', \
-                       help="TDF files that need to be converted to \
-                             JSON-format files.")
-    group.add_argument('-p', '--paths', nargs='+', \
+                       help="TDF files that need to be added to ElasticSearch.")
+    group.add_argument('-d', '--dirs', nargs='+', \
                        help="Directories containing TDF files that need to be \
-                             converted to JSON-format files.  If -ext is not \
+                             added to ElasticSearch.  If -ext is not \
                              specified, only files ending in " + \
                              default_extensions_string + " will be processed.")
-    group.add_argument('-j2es', '--json_to_es', nargs='+', \
-                       help="Add the specified JSON-format file(s) to \
-                       an ElasticSearch database.")
     group.add_argument('-test', '--run_test', \
                         action="store_true", default=False, \
                         help="Test query to verify presence of data.")
+    group.add_argument('-i', '--get_indices', action="store_true", \
+                       default=False, help="Get summary of indices.")
 
-    parser.add_argument('-ext', '--input_extensions', nargs='+', \
+    parser.add_argument('-ext', '--extensions', nargs='+', \
                         default=default_input_extensions, \
-                        help="Extensions of files that need to be processed \
-                        when processing an entire directory.  If not \
-                        specified, defaults to " + default_extensions_string)
-    parser.add_argument('-ei', '--es_ip', nargs=1, \
-                        help="IP and port of the ElasticSearch database.")
+                        help="File extensions to process when processing an \
+                              entire directory.  If not specified, defaults \
+                              to " + default_extensions_string)
+    parser.add_argument('-s', '--es_server', nargs=1, required=True, \
+                        help="The ElasticSearch database's server and port.")
+    parser.add_argument('-del', '--delete_preexisting_index', \
+                        action="store_true", default=False, \
+                        help="Before adding docs to an index, delete the index \
+                              if it already exists.")
 
     return parser.parse_args()
 
 
 
 #
-# The main method.  argparser setup lives here for the time being.
+# The main method.
 #
 def main(argv):
     args = set_up_argparser()
@@ -271,27 +219,29 @@ def main(argv):
 
     if args.files is not None:
         for f in args.files:
-            tdf_to_json(f)
-        friendly_list_of_ops.append("converting " + str(len(args.files)) + \
-                                    " file(s)")
+            add_tdf_to_elasticsearch(f, args.es_server, \
+                    delete_preexisting_index=args.delete_preexisting_index)
+        
+        friendly_list_of_ops.append("adding " + str(len(args.files)) + \
+                                    " file(s) to the ElasticSearch database")
 
-    if args.paths is not None:
-        for path in args.paths:
-            convert_all_tdf_in_dir(path, tuple(args.input_extensions))
-            print ""
+    if args.dirs is not None:
+        files_converted = 0
+        for d in args.dirs:
+            files_converted += add_tdfs_in_dir_to_elasticsearch(d, \
+                    args.es_server, extensions=tuple(args.extensions), \
+                    delete_preexisting_indices=args.delete_preexisting_index)
 
-        friendly_list_of_ops.append("converting files in " + \
-                                    str(len(args.paths)) + " directories")
+        friendly_list_of_ops.append("adding " + str(files_converted) + \
+                " files in " + str(len(args.paths)) + " directories to the \
+                 ElasticSearch database")
 
-    if args.es_ip is not None:
-        if args.json_to_es is not None:
-            for f in args.json_to_es:
-                send_json_to_elasticsearch(f, args.es_ip)
-            friendly_list_of_ops.append("sending JSON file to ES database")
+    if args.run_test == True:
+        test_es(args.es_server)
+        friendly_list_of_ops.append("running a test query on the ES database")
 
-        if args.run_test == True:
-            test_es(args.es_ip)
-            friendly_list_of_ops.append("running test query on ES database")
+    if args.get_indices == True:
+        print pyes.ES(args.es_server).get_indices()
 
     operation_elapsed_time = time.time() - operation_start_time
 
@@ -303,7 +253,7 @@ def main(argv):
             # add separator between last op description and this one
             if friendly_list_of_ops[0] is op:    # first item
                 pass
-            elif friendly_list_of_ops[-1] is op: #last item
+            elif friendly_list_of_ops[-1] is op: # last item
                 operation_description += " and "
             else:
                 operation_description += "; "
