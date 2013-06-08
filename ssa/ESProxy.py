caca436c18fd9e20504eda8bb1973471bd8face3
brozeph <joshua.thomas@gmail.com>
2013-06-08 10:52:39 -0700

    converting ESProxy to class in Python for easier reuse

---
+++ ssa/ESProxy.py
@@ -0,0 +1,91 @@
+import json
+import sys
+
+import rawes
+
+
+class ESProxy:
+	#
+	# Creates a "connection" to ES with specified username and password
+	# https://github.com/humangeo/rawes#usage
+	#
+	def __init__(self, es_server, username, password, verify=True):
+		self.es = rawes.Elastic(es_server, auth=(username, password), verify=verify)
+
+
+
+	#
+	# Removes all documents in ES for specified index and type
+	# http://www.elasticsearch.org/guide/reference/api/admin-indices-delete-mapping/
+	#
+	def clear_documents(self, index_name, type_name):
+		path = index_name + "/" + type_name
+		try:
+			return self.es.delete(path)
+		except Exception as e:
+			# ES returns 404 when resource is not found
+			if hasattr(e, 'status_code') and e.status_code == 404:
+				return "index or type did not previously exist"
+			raise e
+
+
+
+	#
+	# Creates an index in ES by specified name if one does not already exist
+	# http://www.elasticsearch.org/guide/reference/api/admin-indices-create-index/
+	#
+	def ensure_index(self, index_name):
+		try:
+			return self.es.put(index_name)
+		except Exception as e:
+			# ES returns generic 400 when index already exists
+			if hasattr(e, 'status_code') and e.status_code == 400:
+				return "index exists"
+			raise e
+
+
+
+	#
+	# Creates a mapping with the specified index and type
+	# http://www.elasticsearch.org/guide/reference/api/admin-indices-put-mapping/
+	#
+	def ensure_mapping(self, index_name, type_name, mapping):
+		path = index_name + "/" + type_name + "/_mapping"
+		return self.es.put(path, data=mapping)
+
+
+
+	#
+	# Persists a Dictionary in ES via bulk index API
+	# http://www.elasticsearch.org/guide/reference/api/bulk/
+	#
+	def bulk_index_docs(self, iterable, index_name, type_name, chunk_size, show_status=None):
+		path = index_name + "/" + type_name + "/_bulk"
+
+		# For bulk ES insert, a command must be present above every data document
+		# that specifies the action to perform. This generator inserts the index
+		# command into an iterable at the appropriate locations.
+		def bulk_index_generator(it, index_name, type_name):
+			for d in it:
+				yield dict(index=dict(_index=index_name, _type=type_name))
+				yield d
+
+		docs = [json.dumps(doc) for doc in bulk_index_generator(iterable, index_name, type_name)]
+		for i in range(0, len(docs), chunk_size):
+			data="\n".join(docs[i:i + chunk_size]) + "\n"
+			self.es.post(path, data=data)
+
+			if show_status is not None:
+				show_status(i + chunk_size, len(docs))
+
+		return "POST " + path + " complete"
+
+
+
+	#
+	# Adds a new document to ES, uses POST to auto-generate an ID
+	# http://www.elasticsearch.org/guide/reference/api/index_/
+	#
+	def index_doc(self, doc, index_name, type_name):
+		path = index_name + "/" + type_name + "/"
+		return self.es.post(path, data=doc)
--- ssa/__main__.py
+++ ssa/__main__.py
@@ -7,7 +7,7 @@ import sys
 import time
 
 # local source dependencies
-import es_proxy
+from ESProxy import ESProxy
 import importfile_util
 
 
@@ -21,74 +21,73 @@ DELIMITER_TAB = "\t"
 # Add the contents of a tab-delimited file (TDF) to an ElasticSearch database.
 #
 def import_data(filename, \
-    es_server, \
-    index_name=None, \
-    type_name=None, \
-    delete_preexisting_type=False, \
-    field_translations=None, \
-    mapping=None, \
-    username=None, \
-    password=None):
-    data_lines = importfile_util.retrieve_file_lines(filename)
+	es_server, \
+	index_name=None, \
+	type_name=None, \
+	delete_preexisting_type=False, \
+	field_translations=None, \
+	mapping=None, \
+	username=None, \
+	password=None):
+	data_lines = importfile_util.retrieve_file_lines(filename)
 
-    if len(data_lines) < 2:
-        print "there is no data to import in " + filename
-        return
+	if len(data_lines) < 2:
+		print "there is no data to import in " + filename
+		return
 
-    if type_name is None:
-        # bugfix - previous code assumed windows file paths
-        #filename_no_path = filename.split('\\')[-1]
-        filename_no_path = os.path.basename(filename)
-        type_name = filename_no_path.split('.')[0].lower()
+	if type_name is None:
+		# bugfix - previous code assumed windows file paths
+		#filename_no_path = filename.split('\\')[-1]
+		filename_no_path = os.path.basename(filename)
+		type_name = filename_no_path.split('.')[0].lower()
 
-    if index_name is None:
-        index_name = type_name
+	if index_name is None:
+		index_name = type_name
 
-    es = es_proxy.get_es(es_server, username, password)
-    full_url = es_server + "/" + index_name + "/" + type_name
-    index_exists = es_proxy.ensure_index(index_name, es)
+	es = ESProxy(es_server, username, password)
+	full_url = es_server + "/" + index_name + "/" + type_name
+	index_exists = es.ensure_index(index_name)
 
-    if delete_preexisting_type:
-        print "clearing existing documents from " + full_url
-        es_proxy.clear_documents(index_name, type_name, es)
+	if delete_preexisting_type:
+		print "clearing existing documents from " + full_url
+		es.clear_documents(index_name, type_name)
 
-    if mapping is not None:
-        print "applying mapping from " + mapping + " to " + full_url
-        mapping_def = importfile_util.retrieve_file(mapping)
-        es_proxy.ensure_mapping(index_name, type_name, mapping_def, es)
+	if mapping is not None:
+		print "applying mapping from " + mapping + " to " + full_url
+		mapping_def = importfile_util.retrieve_file(mapping)
+		es.ensure_mapping(index_name, type_name, mapping_def)
 
-    if index_exists:
-        start_time = time.time()
+	if index_exists:
+		start_time = time.time()
 
-        # translate field names if applicable
-        if field_translations is not None:
-            reader = translate_fields(data_lines, field_translations)
-        else:
-            reader = csv.DictReader(data_lines, delimiter=DELIMITER_TAB)
+		# translate field names if applicable
+		if field_translations is not None:
+			reader = translate_fields(data_lines, field_translations)
+		else:
+			reader = csv.DictReader(data_lines, delimiter=DELIMITER_TAB)
 
-        # closure for displaying status of operation
-        def show_status(current_count, total_count):
-            current_status = current_count * 100 / total_count
-            sys.stdout.write("\rCurrent Status: %d%%" %current_status)
-            sys.stdout.flush()
+		# closure for displaying status of operation
+		def show_status(current_count, total_count):
+			current_status = current_count * 100 / total_count
+			sys.stdout.write("\rCurrent Status: %d%%" %current_status)
+			sys.stdout.flush()
 
-        print "importing data into " + full_url + " from file " + filename
-        es_proxy.bulk_index_docs(reader, \
-            index_name, \
-            type_name, \
-            es, \
-            BULKINDEX_COUNT,
-            show_status)
+		print "importing data into " + full_url + " from file " + filename
+		es.bulk_index_docs(reader, \
+			index_name, \
+			type_name, \
+			BULKINDEX_COUNT,
+			show_status)
 
-        # indicate completion
-        show_status(100, 100)
-        end_time = time.time() - start_time
-        print " operation completed in " + str(end_time) + " seconds"
+		# indicate completion
+		show_status(100, 100)
+		end_time = time.time() - start_time
+		print " operation completed in " + str(end_time) + " seconds"
 
-    else:
-        print "index at " + es_server + "/" + index_name + " can't be written to"
+	else:
+		print "index at " + es_server + "/" + index_name + " can't be written to"
 
-    return
+	return
 
 
 
@@ -97,23 +96,23 @@ def import_data(filename, \
 # translations file.
 #
 def translate_fields(data_lines, field_translations_path):
-    reader = csv.DictReader(data_lines, delimiter=DELIMITER_TAB)
-    fieldtranslation_lines = importfile_util.retrieve_file_lines(field_translations_path)
+	reader = csv.DictReader(data_lines, delimiter=DELIMITER_TAB)
+	fieldtranslation_lines = importfile_util.retrieve_file_lines(field_translations_path)
 
-    if len(fieldtranslation_lines) < 2:
-        return reader
+	if len(fieldtranslation_lines) < 2:
+		return reader
 
-    fieldname_keys = fieldtranslation_lines[0].split(DELIMITER_TAB)
-    fieldname_values = fieldtranslation_lines[1].split(DELIMITER_TAB)
+	fieldname_keys = fieldtranslation_lines[0].split(DELIMITER_TAB)
+	fieldname_values = fieldtranslation_lines[1].split(DELIMITER_TAB)
 
-    # Filters the fields within a Dictionary and maps them to the specified
-    # fieldvalue names so that only fields specified in the field translations
-    # document are returned
-    def data_filter(it, keys, fieldvalues):
-        for d in it:
-            yield dict((fieldvalues[keys.index(k)], d[k]) for k in keys)
+	# Filters the fields within a Dictionary and maps them to the specified
+	# fieldvalue names so that only fields specified in the field translations
+	# document are returned
+	def data_filter(it, keys, fieldvalues):
+		for d in it:
+			yield dict((fieldvalues[keys.index(k)], d[k]) for k in keys)
 
-    return data_filter(reader, fieldname_keys, fieldname_values)
+	return data_filter(reader, fieldname_keys, fieldname_values)
 
 
 
@@ -121,45 +120,45 @@ def translate_fields(data_lines, field_translations_path):
 # Set up the argparser in its own method, for clarity.
 #
 def set_up_argparser():
-    parser = argparse.ArgumentParser(\
-        description='Processes tab-delimited files and imports data to the specified ES server')
-    parser.add_argument('-s', '--es_server', \
-        nargs=1, \
-        required=True, \
-        help="The ES URI")
-    parser.add_argument('-user', '--username', \
-        nargs=1, \
-        default=[None], \
-        help="Username for ES")
-    parser.add_argument('-pass', '--password', \
-        nargs=1, \
-        default=[None], \
-        help="Password for ES")
-    parser.add_argument('-i', '--index_name', \
-        nargs=1, \
-        required=True, \
-        help="The index for the new data.")
-    parser.add_argument('-rm', '--delete_preexisting_type', \
-        action="store_true", \
-        default=False, \
-        help="Remove any docs of specified type prior to import")
-    parser.add_argument('-f', '--filename', \
-        nargs=1, \
-        help="Tab-delimited file to import into ES")
-    parser.add_argument('-map', '--map_file_path', \
-        nargs=1, \
-        default=[None], \
-        help="Mapping file for specified type")
-    parser.add_argument('-fn', '--field_name_translations_path', \
-        nargs=1, \
-        default=[None], \
-        help="Translation file between mapping and tab-delimited columns.")
-    parser.add_argument('-t', '--type_name', \
-        nargs=1, \
-        default=[None], \
-        help="The type for the documents to import.")
+	parser = argparse.ArgumentParser(\
+		description='Processes tab-delimited files and imports data to the specified ES server')
+	parser.add_argument('-s', '--es_server', \
+		nargs=1, \
+		required=True, \
+		help="The ES URI")
+	parser.add_argument('-user', '--username', \
+		nargs=1, \
+		default=[None], \
+		help="Username for ES")
+	parser.add_argument('-pass', '--password', \
+		nargs=1, \
+		default=[None], \
+		help="Password for ES")
+	parser.add_argument('-i', '--index_name', \
+		nargs=1, \
+		required=True, \
+		help="The index for the new data.")
+	parser.add_argument('-rm', '--delete_preexisting_type', \
+		action="store_true", \
+		default=False, \
+		help="Remove any docs of specified type prior to import")
+	parser.add_argument('-f', '--filename', \
+		nargs=1, \
+		help="Tab-delimited file to import into ES")
+	parser.add_argument('-map', '--map_file_path', \
+		nargs=1, \
+		default=[None], \
+		help="Mapping file for specified type")
+	parser.add_argument('-fn', '--field_name_translations_path', \
+		nargs=1, \
+		default=[None], \
+		help="Translation file between mapping and tab-delimited columns.")
+	parser.add_argument('-t', '--type_name', \
+		nargs=1, \
+		default=[None], \
+		help="The type for the documents to import.")
 
-    return parser
+	return parser
 
 
 
@@ -167,37 +166,37 @@ def set_up_argparser():
 # The main method.
 #
 def main(argv):
-    argparser = set_up_argparser()
-    args = argparser.parse_args()
-    es_server = args.es_server[0]
+	argparser = set_up_argparser()
+	args = argparser.parse_args()
+	es_server = args.es_server[0]
 
-    if args.index_name is not None:
-        index_name = args.index_name[0]
-    else:
-        index_name = None
+	if args.index_name is not None:
+		index_name = args.index_name[0]
+	else:
+		index_name = None
 
-    if args.filename is not None:
-        if os.path.isfile(args.filename[0]):
-            if args.type_name is not None:
-                type_name = args.type_name[0]
-            else:
-                type_name = None
+	if args.filename is not None:
+		if os.path.isfile(args.filename[0]):
+			if args.type_name is not None:
+				type_name = args.type_name[0]
+			else:
+				type_name = None
 
-            import_data(args.filename[0], \
-                es_server, \
-                index_name=index_name, \
-                type_name=type_name, \
-                mapping=args.map_file_path[0], \
-                field_translations=args.field_name_translations_path[0], \
-                delete_preexisting_type=args.delete_preexisting_type, \
-                username = args.username[0], \
-                password = args.password[0])
+			import_data(args.filename[0], \
+				es_server, \
+				index_name=index_name, \
+				type_name=type_name, \
+				mapping=args.map_file_path[0], \
+				field_translations=args.field_name_translations_path[0], \
+				delete_preexisting_type=args.delete_preexisting_type, \
+				username = args.username[0], \
+				password = args.password[0])
 
-            sys.exit(0)
-        else:
-            print "Error: filename argument supplied is not a file"
-            argparser.print_help()
-            sys.exit(1)
+			sys.exit(0)
+		else:
+			print "Error: filename argument supplied is not a file"
+			argparser.print_help()
+			sys.exit(1)
 
 
 
@@ -205,4 +204,4 @@ def main(argv):
 # Run as module
 #
 if __name__ == "__main__":
-    main(sys.argv[1:])
+	main(sys.argv[1:])
